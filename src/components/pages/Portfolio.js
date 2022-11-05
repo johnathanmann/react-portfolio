b2a86cbea17a52c9f7ec50c51907ba1a3b9fe0f5
Johnny <johnathan.mnn@gmail.com>
2022-11-05 16:37:29 -0500

    Added portfolio page adn fixed navbar issue

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,16 +2,48 @@ import React from 'react';
 import '../../styles/Portfolio.css';
 import PortfolioCard from '../PortfolioCard';
 import projects from '../../assets/projects.json'
+import seared from '../../assets/seared.png'
+import weather from '../../assets/weather.png'
+import workout from '../../assets/workout.png'
+import planner from '../../assets/planner.png'
+import password from '../../assets/password.png'
+import quiz from '../../assets/quiz.png'
 
 export default function Portfolio() {
   return(
-    <div class="container text-center">
-    <h1 id='header text-left'>Portfolio</h1>
+    <div className="containerr">
+    <h1 className='header text-left'>Portfolio</h1>
+    <div className='text-center'>
     <PortfolioCard
       name={projects[0].name}
-      img={projects[0].img}
+      img={seared}
       link={projects[0].link}
     />
+    <PortfolioCard
+      name={projects[1].name}
+      img={weather}
+      link={projects[1].link}
+    />
+    <PortfolioCard
+      name={projects[2].name}
+      img={workout}
+      link={projects[2].link}
+    />
+    <PortfolioCard
+      name={projects[3].name}
+      img={planner}
+      link={projects[3].link}
+    />
+    <PortfolioCard
+      name={projects[4].name}
+      img={password}
+      link={projects[4].link}
+    />
+    <PortfolioCard
+      name={projects[5].name}
+      img={quiz}
+      link={projects[5].link}
+    /></div>
     </div>
   )
 }
