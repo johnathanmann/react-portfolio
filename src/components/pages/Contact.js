a6339042e7ef62f9d47ac9bd0228fe484d0ccbb5
Johnny <johnathan.mnn@gmail.com>
2022-11-07 20:53:33 -0600

    Form now requires appropriate input

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -9,15 +9,15 @@ export default function Contact() {
       <form>
       <div className="form-group">
     <label for="name">Name</label>
-    <input type="email" className="form-control"  placeholder="Your Name"/>
+    <input type="text" className="form-control"  placeholder="Your Name" required/>
   </div>
   <div className="form-group">
     <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" className="form-control"  placeholder="name@example.com"/>
+    <input type="email" className="form-control"  placeholder="name@example.com" required/>
   </div>
       <div className="form-group">
-        <label for="exampleFormControlTextarea1">Message:</label>
-        <textarea className="form-control"  rows="3"></textarea>
+        <label for="text">Message:</label>
+        <textarea className="form-control"  rows="3" required></textarea>
       </div>
       <button type="submit" class="btn btn-danger ms-4">Submit</button>
     </form>
